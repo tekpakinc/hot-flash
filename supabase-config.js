b29d97b9e43a51b9205903f6769dd2fef8e742c7
tekpakinc <info.tekpak@gmail.com>
2026-07-09 01:18:54 -0400

    Update Supabase anon key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 const HOTFLASH_SUPABASE_URL = "https://juwkzmlchffbovlrqhex.supabase.co";
-const HOTFLASH_SUPABASE_ANON_KEY = "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE";
+const HOTFLASH_SUPABASE_ANON_KEY = "sb_publishable_mWZZuoYhHOb4ivMpxZNuHA_DeUKW_zB";
 
 const hotflashSupabase = window.supabase.createClient(
   HOTFLASH_SUPABASE_URL,
