97d560eaeee5d6a0f1e460eae2ae3f7abeea9751
tekpakinc <info.tekpak@gmail.com>
2026-07-12 13:29:52 -0400

    Add user music autoplay preference

--- supabase-vehicle-types-music.sql
+++ supabase-vehicle-types-music.sql
@@ -1,5 +1,6 @@
 alter table vehicles add column if not exists vehicle_type text default 'automobile';
 alter table vehicles add column if not exists music_url text;
 alter table vehicles add column if not exists music_title text;
+alter table profiles add column if not exists music_autoplay boolean not null default false;
 
 update vehicles set vehicle_type='automobile' where vehicle_type is null;
